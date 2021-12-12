b82b5f4ec991169b81e473ff80a3ccf008c96141
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2021-12-12 14:30:12 +0100

    [Fix] Complete Landing page

--- src/components/AdminLogistics/AdminLogistics.js
+++ src/components/AdminLogistics/AdminLogistics.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Card } from '../UI/Card/Card'
+import classes from './AdminLogistics.module.css'
+import icon1 from '../../assets/images/Logistics.svg'
+import icon2 from '../../assets/images/asset.svg'
+import DirectReports from '../../assets/images/Direct Reports-List.png'
 
 export const AdminLogistics = () => {
     return (
-        <div>
-            <h1>AdminLogistics</h1>
+        <div className={classes.admin_section}>
+            <div className={classes.admin_section_container}>
+                <div className={classes.admin_section_text}>
+                    <h2 className={classes.admin_title}>Admin & Logistics</h2>
+                    <p>Manage and track company assets as well as logistics for travelling employees</p>
+                </div>
+                <div className={classes.admin_section_cards}>
+                    <Card className={classes.hr_content__card}
+                        title="Travel & Logistics"
+                        description="Make travel requests, get approvals, and have access to travel information."
+                        icon={icon1}
+                    />
+                    <Card className={classes.hr_content__card}
+                        title="Asset Management"
+                        description="Manage the acquisition, assignment, and disposition of assets seamlessly."
+                        icon={icon2}
+                    />
+                </div>
+            </div>
+            <div className={classes.admin_snapshot}>
+                <img src={DirectReports} alt="Admin Snapshot" />
+            </div>
         </div>
     )
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,47 @@
 import React from 'react'
+import { Container } from '../UI/Container/Container';
+import { Image } from '../UI/Img/Image';
+import logo from '../../assets/images/Genesys_logo.png';
+import classes from './Footer.module.css'
+import { Link } from 'react-router-dom';
+import facebook from '../../assets/icons/facebook.svg';
+import twitter from '../../assets/icons/twitter.svg';
+import linkedin from '../../assets/icons/linkedin.svg';
+import instagram from '../../assets/icons/instagram.svg';
 
 const Footer = () => {
     return (
         <div>
-            <footer className="footer">
-                <div className="container">
-                    <div className="content has-text-centered">
-                        <p>
-                            <strong>React</strong> by <a href='/'>Paul</a>
-                        </p>
+            <footer className={classes.footer}>
+                <Container className={classes.footer_container}>
+                    <div className={classes.logo}>
+                        <Link to="/">
+                            <Image src={logo} alt="logo" />
+                        </Link>
+                        <p className={classes.text}>Throw paperwork into the trash.</p>
                     </div>
-                </div>
+                    <div className={classes.footer_right}>
+                        <div className={classes.social}>
+                            <ul>
+                                <li><Link to="/">
+                                    <Image src={facebook} alt="facebook" />
+                                </Link></li>
+                                <li><Link to="/">
+                                    <Image src={twitter} alt="twitter" />
+                                </Link></li>
+                                <li><Link to="/">
+                                    <Image src={linkedin} alt="linkedin" />
+                                </Link></li>
+                                <li><Link to="/">
+                                    <Image src={instagram} alt="instagram" />
+                                </Link></li>
+                            </ul>
+                        </div>
+                        <div className={classes.footer_copyright}>
+                            <p>Copyright © Genesys DevStudio. All rights reserved</p>
+                        </div>
+                    </div>
+                </Container>
             </footer>
         </div>
     )
